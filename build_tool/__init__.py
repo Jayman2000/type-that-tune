1f54f25c770406b8d2e65c09f5b2b231365cf951
Jason Yundt <jason@jasonyundt.email>
2024-08-27 09:34:16 -0400

    Create build tool
    At the moment, the build tool doesn’t really do anything (it just prints
    “Finished!”). Future commits will make the build tool actually generate
    files that need to exist before the project gets imported by Godot.

---
+++ build_tool/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
+def main() -> int:
+    print("Finished!")
+    return 0
