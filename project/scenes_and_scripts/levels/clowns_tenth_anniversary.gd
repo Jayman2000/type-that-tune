e5ae2a5c8c54257d80e75a940f81157efbb13b04
Jason Yundt <jason@jasonyundt.email>
2024-08-28 08:44:53 -0400

    Teach player how to pause and unpause

--- project/scenes_and_scripts/levels/clowns_tenth_anniversary.gd
+++ project/scenes_and_scripts/levels/clowns_tenth_anniversary.gd
@@ -3,11 +3,30 @@
 extends "res://scenes_and_scripts/base_level.gd"
 
 
+const PauseMenu := preload("res://scenes_and_scripts/pause_menu.gd")
+
+var player_has_confirmed_they_know_how_to_pause: bool = false
+var waiting_for_player_to_pause: bool = false
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var pause_menu: PauseMenu = $PauseMenu
+@onready var tutorial_text_box: RichTextLabel = \
+    $TutorialContainer/Panel/MarginContainer/TutorialTextBox
 @onready var video_stream_player: VideoStreamPlayer = $VideoStreamPlayer
 
 
-func set_up_part_1_signals() -> void:
+func set_up_for_part_1() -> void:
+    tutorial_text_box.text = (
+        "[center]Welcome to Type That Tune![/center]\n\n"
+        + "You can pause and unpause the game at any time by pressing "
+        + "one of these keys:\n\n"
+    )
+    for event: InputEvent in InputMap.action_get_events(&"pause"):
+        tutorial_text_box.text += "• " + event.as_text() + "\n"
+    tutorial_text_box.text += \
+        "\nTry pausing and unpausing the game now."
+
+
     text_queue_completed.connect(
         func(): animation_player.play(&"part_2"),
         CONNECT_ONE_SHOT
@@ -16,6 +35,21 @@ func set_up_part_1_signals() -> void:
         func(): $TutorialContainer/FadeOutPlayer.play(&"fade_out"),
         CONNECT_ONE_SHOT
     )
+
+
+func switch_to_second_message_for_part_1() -> void:
+    tutorial_text_box.text = (
+        "Good job!\n\n"
+        + "Whenever you see text appear in the center of your screen, "
+        + "type it in, and then press ENTER. Make sure that you "
+        + "capitalize everything correctly."
+    )
+
+
+func make_sure_player_knows_how_to_pause() -> void:
+    if not player_has_confirmed_they_know_how_to_pause:
+        animation_player.pause()
+        waiting_for_player_to_pause = true
 
 
 func wait_for_text_queue_to_be_finished(
@@ -46,8 +80,16 @@ func _unhandled_input(event: InputEvent) -> void:
         if animation_player.is_playing():
             if animation_player.current_animation == "part_1":
                 # Skip to the end of the tutorial:
+                player_has_confirmed_they_know_how_to_pause = true
                 animation_player.advance(
                     animation_player.current_animation_length
                 )
                 get_viewport().set_input_as_handled()
                 return
+
+
+func _on_pause_menu_unpaused():
+    player_has_confirmed_they_know_how_to_pause = true
+    if waiting_for_player_to_pause:
+        waiting_for_player_to_pause = false
+        animation_player.play()
--- project/scenes_and_scripts/pause_menu.gd
+++ project/scenes_and_scripts/pause_menu.gd
@@ -3,12 +3,16 @@
 extends ColorRect
 
 
+signal unpaused
+
+
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("pause"):
         var scene_tree: SceneTree = get_tree()
         if scene_tree.paused:
             visible = false
             scene_tree.paused = false
+            unpaused.emit()
         else:
             visible = true
             scene_tree.paused = true
