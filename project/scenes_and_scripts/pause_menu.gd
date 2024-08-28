b7a4893e93f7cca5478d2b4d5d62ada58bcbc761
Jason Yundt <jason@jasonyundt.email>
2024-08-28 07:34:17 -0400

    Add pause menu
    One of the reasons why I’m adding this now is to make the game easier to
    debug. In a future commit, I’m going to add an option to the pause menu
    that will allow you to skip ahead in the level. That option will make it
    easier for me to fix a problem where a specific song title appears too
    early.

---
+++ project/scenes_and_scripts/pause_menu.gd
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
+extends ColorRect
+
+
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("pause"):
+        var scene_tree: SceneTree = get_tree()
+        if scene_tree.paused:
+            visible = false
+            scene_tree.paused = false
+        else:
+            visible = true
+            scene_tree.paused = true
+        get_viewport().set_input_as_handled()
