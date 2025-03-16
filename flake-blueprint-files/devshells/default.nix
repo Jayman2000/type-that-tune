98c60674052febb5e2d7ec9302c36e7ffd4e5fea
Jason Yundt <jason@jasonyundt.email>
2025-03-16 17:39:04 -0400

    Make this repo’s flake use blueprint
    I find that blueprint [1] makes it easier for me to create an add on to
    flakes. The main motivation behind this change is to make it easier to
    create a future commit. That future commit will add additional outputs
    to this repo’s flake.
    
    [1]: <https://github.com/numtide/blueprint>

---
+++ flake-blueprint-files/devshells/default.nix
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024–2025 Jason Yundt <jason@jasonyundt.email>
+{
+  perSystem,
+  pkgs,
+}:
+pkgs.mkShellNoCC {
+  name = "dev-shell-for-type-that-tune";
+  packages = [
+    pkgs.git
+    pkgs.nodePackages_latest.livedown
+    perSystem.nixpkgsOldStable.godot_4
+    pkgs.ffmpeg
+    pkgs.uv
+
+    # Dependencies for ttt-build-tool
+    pkgs.pkg-config
+
+    pkgs.pre-commit
+    # Dependencies for pre-commit hooks:
+    pkgs.go
+    perSystem.nixpkgsUnstable.rustc
+    pkgs.cabal-install
+    pkgs.ghc
+  ];
+}
