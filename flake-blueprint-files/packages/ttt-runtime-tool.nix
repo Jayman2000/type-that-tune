092b85b386d596f2b60e5e2d484496b33ef372e6
Jason Yundt <jason@jasonyundt.email>
2025-03-17 09:06:35 -0400

    Create Nix package for ttt-runtime-tool
    A future commit is going to make the ttt-build-tool depend on the
    ttt-runtime-tool. When I make that commit, I’m going to have to update
    the ttt-build-tool Nix package so that it depends on a Nix package for
    the ttt-runtime-tool. I’m creating the ttt-runtime-tool Nix package now
    so that I don’t have to do it later.

---
+++ flake-blueprint-files/packages/ttt-runtime-tool.nix
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+{
+  flake,
+  pkgs,
+  pname,
+}:
+pkgs.python3Packages.buildPythonApplication {
+  inherit pname;
+  version =
+    let
+      pyprojectContents = builtins.readFile "${flake}/pyproject.toml";
+      parsedPyprojectContents = builtins.fromTOML pyprojectContents;
+    in
+    parsedPyprojectContents.project.version;
+  src = "${flake}/python_distribution_packages/ttt-runtime-tool";
+  pyproject = true;
+
+  build-system = [ pkgs.python3Packages.setuptools ];
+}
