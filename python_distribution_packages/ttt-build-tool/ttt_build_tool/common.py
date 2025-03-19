0f906e01c68c384d63c3509d372780c7e381beb0
Jason Yundt <jason@jasonyundt.email>
2025-03-19 11:13:45 -0400

    Remove unused stuff
    I’m keeping common.run_command, though because I’m probably going to end
    up using it in the future. I’m not really sure whether or not I’m going
    to use the rest of the stuff, though, so I’m getting rid of it.

--- python_distribution_packages/ttt-build-tool/ttt_build_tool/common.py
+++ python_distribution_packages/ttt-build-tool/ttt_build_tool/common.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2024–2025 Jason Yundt <jason@jasonyundt.email>
-import os
 import pathlib
 import subprocess
 import sys
@@ -10,16 +9,6 @@ from typing import Final, NamedTuple, Union
 
 GODOT_PROJECT_DIR: Final = pathlib.Path("godot_project")
 GENERATED_DIR: Final = pathlib.Path(GODOT_PROJECT_DIR, "generated")
-GODOT_ENGINE_DIR: Final = pathlib.Path("godot_engine")
-GODOT_SRC_DIR: Final = pathlib.Path(GODOT_ENGINE_DIR, "src")
-GODOT_BUILD_BIN_DIR: Final = pathlib.Path(
-    GODOT_SRC_DIR,
-    "bin"
-)
-EDITOR_EXECUTABLE_SYMLINK_PATH: Final = pathlib.Path(
-    GODOT_ENGINE_DIR,
-    "editor.exe" if os.name == "nt" else "editor"
-)
 
 
 class Settings(NamedTuple):
@@ -38,11 +27,3 @@ def run_command(
     )
     if RESULT.returncode != 0:
         raise RuntimeError(f"This command failed: {command}")
-
-
-def remove_then_symlink(
-    target: pathlib.Path,
-    symlink_path: pathlib.Path
-) -> None:
-    symlink_path.unlink(missing_ok=True)
-    symlink_path.symlink_to(target)
