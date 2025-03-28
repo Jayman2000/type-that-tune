83de7742410ed0795fa1ec8ade9c618f9a3802de
Jason Yundt <jason@jasonyundt.email>
2025-03-28 19:18:43 -0400

    Fix ttt-build-tool Nix package
    Before this change, the ttt-build-tool Nix package would fail to build.

--- flake-blueprint-files/packages/ttt-build-tool.nix
+++ flake-blueprint-files/packages/ttt-build-tool.nix
@@ -20,6 +20,7 @@ pkgs.python3Packages.buildPythonApplication {
   build-system = [ pkgs.python3Packages.setuptools ];
   dependencies = with pkgs.python3Packages; [
     appdirs
+    requests-cache
     reuse
     yt-dlp
   ];
