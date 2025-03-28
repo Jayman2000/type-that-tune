d19b04c0c88807da05273a9097ac5d41dc7f62b6
Jason Yundt <jason@jasonyundt.email>
2025-03-28 19:10:23 -0400

    Make python -m ttt_build_tool work
    Sometimes when I run the ttt-build-tool, it uses so much memory that my
    system starts swapping. I have 64 GB of RAM!! I want to use Scalene [1]
    to try and figure out why this is happening, but scalene doesn’t work
    with console scripts. I need to be able to run my program by doing
    either “python script.py” or “python -m module_name”. Otherwise, I won’t
    be able to use Scalene on my program.
    
    [1]: <https://github.com/plasma-umass/scalene>

---
+++ python_distribution_packages/ttt-build-tool/ttt_build_tool/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+import sys
+
+from . import main
+
+
+sys.exit(main())
